e351ed88ce4677dc333b5de86fdef1a343f12d1c
simo.zgaoua <m.zgaoua2001@gmail.com>
2023-02-26 00:19:42 +0100

    book class

---
+++ app.js
@@ -0,0 +1,18 @@
+//Book Class : Represents a Book
+class Book{
+    constractor(title,auteur,isbn){
+       this.title = title;
+       this.auteur = auteur;
+       this.isbn = isbn;
+    }
+}
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Storage
+
+//Event : display Book
+
+//Event : add Book
+
+//Event : remove Book
