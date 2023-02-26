460c5c20aadd8636305367ffb7beb68b46daf2df
simo.zgaoua <m.zgaoua2001@gmail.com>
2023-02-26 15:36:59 +0100

    displayBooks method

--- app.js
+++ app.js
@@ -9,9 +9,45 @@ class Book{
 
 // UI Class: Handle UI Tasks
 
+class UI{
+    static displayBooks(){
+        const StoredBooks = [
+            {
+                title : 'Book One',
+                auteur : 'simo zgaoua',
+                isbn : '123456'
+            },
+            {
+                title : 'Book Two',
+                auteur: 'jane doe',
+                isbn : '98765'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book){
+        const list = document.querySelector('#book-list');
+        const row = document.createElement('tr');
+        
+        row.innerHTML = `
+          <td>${book.title}</td>
+          <td>${book.auteur}</td>
+          <td>${book.isbn}</td>
+          <td><a href="#" class = "btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
+
 // Store Class: Handles Storage
 
 //Event : display Book
+document.addEventListener('DOMContentLoaded',UI.displayBooks);
 
 //Event : add Book
 
